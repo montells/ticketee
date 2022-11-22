30294c08afad5c8d980203496e9015934cfcd97f
Michel Sánchez Montells <montells@aleph.engineering>
2022-11-22 20:59:12 +0100

    Add validation to ensure names are specified when creating projects

--- spec/features/creating_projecs_spec.rb
+++ spec/features/creating_projecs_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can create a new project' do
-  scenario 'with valid attributes' do
+  before do
     visit '/'
 
     click_link 'New Project'
+  end
 
+  scenario 'with valid attributes' do
     fill_in :project_name, with: 'Visual Studio Code'
     fill_in :project_description, with: 'Code editing redefined'
     click_button 'Create Project'
@@ -17,5 +19,12 @@ RSpec.feature 'Users can create a new project' do
 
     title = 'Visual Studio Code - Projects - Ticketee'
     expect(page).to have_title title
+  end
+
+  scenario 'with provided in valid attributes' do
+    click_button 'Create Project'
+
+    expect(page).to have_content 'Project has not been created.'
+    expect(page).to have_content "Name can't be blank"
   end
 end
