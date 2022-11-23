d3ed8fedc954b3001ac9cda4d86a330d8cc78689
Michel Sánchez Montells <montells@aleph.engineering>
2022-11-23 11:40:20 +0100

    Projects can now be updated

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can view projects' do
+  before do
+    FactoryBot.create :project, {name: 'Visual Studio Code'}
+
+    visit '/'
+    click_link 'Visual Studio Code'
+    click_link 'Edit Project'
+  end
+
+  scenario 'with the project details' do
+    fill_in 'project_name', with: 'VS Code'
+    click_button 'Update Project'
+
+    expect(page).to have_content('Project has been updated')
+    expect(page).to have_content('VS Code')
+  end
+
+  scenario 'when providing invalid attributes' do
+    fill_in 'project_name', with: ''
+    click_button 'Update Project'
+
+    expect(page).to have_content('Project has not been updated')
+  end
+end
