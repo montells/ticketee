c509daa0732758490fdeec09a05e884efbdcef66
Michel Sánchez Montells <montells@aleph.engineering>
2022-12-27 05:02:26 +0100

    Archived users cannot sign in to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,15 @@ class User < ApplicationRecord
     "#{email} (#{admin? ? 'Admin' : 'User'})"
   end
 
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+
   def archive!
     self.update(archived_at: Time.now)
+  end
+
+  def inactive_message
+    archived_at.nil? ? super : :archived
   end
 end
--- spec/features/singing_in_spec.rb
+++ spec/features/singing_in_spec.rb
@@ -13,4 +13,17 @@ RSpec.feature 'Users can sign in' do
     expect(page).to have_content('Signed in successfully.')
     expect(page).to have_content("Signed in as #{user.email}")
   end
+
+  scenario 'unless they are archived' do
+    user.archive!
+
+    visit '/'
+    click_link 'Sing in'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+
+    click_button 'Log in'
+    expect(page).to have_content('Your account has been archived.')
+  end
 end
